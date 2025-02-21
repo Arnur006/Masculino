bb91fd27461f4981bfcb6aab91316a89ca2292ce
Arnur006 <arnurdzhalimbetov@mail.ru>
2025-02-21 08:44:10 +0300

    Global Update

--- js/profil.js
+++ js/profil.js
@@ -1,4 +1,7 @@
 const content = document.querySelector(".content")
+user = JSON.parse(localStorage.getItem('user'))
+const login = document.querySelector(".Login-user")
+login.textContent=user.userLogin
 const func = {
     Dashboard: (el)=>{
         const c =`<div class="flexx"><p class="From"  >From your account dashboard you can view your <span class="blue" id="Orders">recent orders</span>, manage your <span class="blue" id="Adress">billing address</span>, and <span class="blue" id="AccountDetails">edit your password and account details</span>.</p></div>`
@@ -37,37 +40,37 @@ const func = {
         el.classList.add('active')
     },
     AccountDetails: (el)=>{
-        const c =`<div class="iop-2 ass">
-      <div class="form-groupe">
-        <label class="male" for="firstname">First name *</label>
-        <input id="firstname" class="black blackkk" type="text">
+        const c =`<div class="iop-2 ass ppoo">
+      <div class="form-groupe ppoo">
+        <label class="male ppoopp" for="firstname">First name *</label>
+        <input id="firstname" class="black blackkk ppoo" type="text">
       </div>
-      <div class="form-groupe">
-        <label class="male" for="lastname">Last name *</label>
-        <input id="lastname" class="black blackkk" type="text">
+      <div class="form-groupe ppoo">
+        <label class="male ppoo"  for="lastname">Last name *</label>
+        <input id="lastname" class="black blackkk ppoo" type="text">
       </div>
     </div>
     
-    <div class="iop-3 ass form-groupe">
-      <label class="male" for="street">Display name *</label>
-      <input id="street" class="black blackk" type="text">
-      <p class="ogo">This will be how your name will be displayed in the account section and in reviews</p>
+    <div class="iop-3 ass form-groupe ppoopp">
+      <label class="male ppoopp" for="street">Display name *</label>
+      <input id="street" class="black blackk ppoopp" type="text">
+      <p class="ogo ppoopp">This will be how your name will be displayed in the account section and in reviews</p>
     </div>
-    <div class="iop-4 ass form-groupe">
-      <label class="male" for="city">Email adress *</label>
-      <input id="city" class="black blackk" type="text">
+    <div class="iop-4 ass form-groupe ppoopp">
+      <label class="male ppoopp" for="city">Email adress *</label>
+      <input id="city" class="black blackk ppoopp" type="text">
     </div>
-    <div class="iop-5 ass form-groupe">
-      <label class="male" for="country">Current password (leave blank to leave unchanged) *</label>
-      <input id="country" class="black blackk" type="text">
+    <div class="iop-5 ass form-groupe ppoopp">
+      <label class="male ppoopp" for="country">Current password (leave blank to leave unchanged) *</label>
+      <input id="country" class="black blackk ppoopp" type="text">
     </div>
-    <div class="iop-6 ass form-groupe">
-      <label class="male" for="zip">New password (leave blank to leave unchanged) *</label>
-      <input id="zip" class="black blackk" type="text">
+    <div class="iop-6 ass form-groupe ppoopp">
+      <label class="male ppoopp" for="zip">New password (leave blank to leave unchanged) *</label>
+      <input id="zip" class="black blackk ppoopp" type="text">
     </div>
-    <div class="iop-7 ass form-groupe">
-      <label class="male" for="phone">Confirm new password *</label>
-      <input id="phone" class="black blackk" type="text">
+    <div class="iop-7 ass form-groupe ppoopp">
+      <label class="male ppoopp" for="phone">Confirm new password *</label>
+      <input id="phone" class="black blackk ppoopp" type="text">
     </div>
     <div class="SSAA">
     <button class="Save">Save changes</button>
@@ -88,3 +91,12 @@ p.forEach(el =>{
         func[el.id](el)
     })
 })
+let activeEl= null
+p.forEach(el=>{
+  if (el.textContent==="Dashboard"){
+    activeEl=el
+  }
+
+})
+func.Dashboard(activeEl)
+console.log(localStorage)
--- js/register.js
+++ js/register.js
@@ -1,12 +1,46 @@
 const lost = document.querySelector(".lost")
+const login = document.querySelector("#login")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
+const entername = document.querySelector(".enter-name")
 lost.addEventListener('submit', (e) => {
     e.preventDefault()
-    const user = {
-        userEmail : email.value,
-        userPassword : password.value,
+    if(login.value.trim() ===""){
+        entername.textContent = 'Error: Username is required.'
+        entername.classList.remove("enter-name-none")
     }
-    localStorage.setItem("user", JSON.stringify(user))
-    window.location.href="./profil.html"
+    else if(email.value.trim() ===""){
+        entername.textContent = 'Invalid email.'
+        entername.classList.remove("enter-name-none")
+    }
+    else if(password.value.trim() ===""){
+        entername.textContent = 'Invalid password'
+        entername.classList.remove("enter-name-none")
+    }
+    else{
+        entername.classList.add("enter-name-none")
+        const user = {
+            userLogin : login.value,
+            userEmail : email.value,
+            userPassword : password.value,
+        }
+        localStorage.setItem("user", JSON.stringify(user))
+        window.location.href="./profil.html"
+    }
 })
+
+const respas = document.querySelector(".res-pas")
+  respas.addEventListener("click" , ()=>{
+    if(login.value.trim() ===""){
+        entername.textContent = 'Error: Username is required.'
+        entername.classList.remove("enter-name-none")
+    }
+    else if(email.value.trim() ===""){
+        entername.textContent = 'Invalid email.'
+        entername.classList.remove("enter-name-none")
+    }
+    else if(password.value.trim() ===""){
+        entername.textContent = 'Invalid password'
+        entername.classList.remove("enter-name-none")
+    }
+  })
