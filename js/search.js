b38a07d35d3b0bb17bac81bcfe1e4b79985c2b5b
Arnur006 <arnurdzhalimbetov@mail.ru>
2025-02-28 17:25:30 +0300

    Global update

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-const cards = {
+const carrdds = {
     'Водолазка(normal)':{
         img:'католог/Водолазка(normal)/1.jpg',
         price:"13,50$",
@@ -9,7 +9,7 @@ const cards = {
         price:'49,00$',
          href: "./Куртка(normal).html"
     },
-    'Водолазка(normal)':{
+    'Куртка(normal)':{
         img:'католог/Куртка(normal)(2)/1.jpg',
         price:"49,50$",
         href: "./Куртка(normal)(2).html"
@@ -50,41 +50,81 @@ const cards = {
          href: "./Мужской кардиган (normal).html"
     },
     }
+    const ooop = document.querySelector(".ooop")
 const searchResultAdd = ()=>{
-    if (window.innerWidth>992){
+    // if (window.innerWidth>992){
         const searchresult = document.querySelector(".search-result")
        if(!searchresult){
         const div = document.createElement("div")
         div.classList.add("search-result")
         div.innerHTML = `
-         <h5 class="searh-title">Showing all results:</h5>
+         <h5 class="searh-title"></h5>
                                 <ul class="search-list" >
     
                                 </ul>
         `
         searchModal.appendChild(div)
+        div.style.display = 'none'
+        const searhtitle = document.querySelector(".searh-title")
+        const searchlist = document.querySelector(".search-list")
+        search(ooop,searchlist,searhtitle,)
        }
     
+    // }
+    // else{
+    //     const searchresult = document.querySelector(".search-result")
+    //     if(searchresult){
+    //        searchModal.removeChild(searchresult) 
+    //     }
+        
+    // }
+}
+const search = (input,list,title,result)=>{
+input.addEventListener("input",()=>{
+    const searchresult = document.querySelector(".search-result")
+    console.log(searchresult)
+    let content =  ''
+    let filtredCards = Object.keys(carrdds).filter(key=>key.trim().toLowerCase().includes(input.value.trim().toLowerCase()))
+    filtredCards.forEach(key=>{
+        content+= `
+        <li class="search-card">
+                                    <a href="${carrdds[key].href}" class="search-link">
+                                      <img  class="custom-card-image" src="${carrdds[key].img}" alt="">
+                                      <h5 class="card-title search-card-title">${key}</h5>
+                                      <p class="card-text custom-card-text search-card-price">${carrdds[key].price}</p>
+
+                                    </a>
+                                  </li>
+        `
+    })
+    searchresult.style.display='block'
+    list.innerHTML=content
+    if(input.value.trim() === ""){
+        content = ""
+        filtredCards=[]
+        list.innerHTML=content
+        searchresult.style.display='none'
+    }
+    if(filtredCards.length<1){
+        title.textContent = 'Нет совпадений'
     }
     else{
-        const searchresult = document.querySelector(".search-result")
-        if(searchresult){
-           searchModal.removeChild(searchresult) 
-        }
-        
+        title.textContent = 'Showing all results:'
     }
+    
+})
 }
+const gc = document.querySelector(".gc")
+gc.addEventListener('click',()=>{
+    const searchresult = document.querySelector(".search-result")
+    const list = searchresult.querySelector('.search-list')
+     searchresult.style.display='none'
+     list.innerHTML = ''
+     ooop.value = ''
+})
 const searchModal = document.querySelector(".search-modal")
 window.addEventListener("resize",searchResultAdd)
 document.addEventListener("DOMContentLoaded",searchResultAdd)
 
 
 
-// <li class="search-card">
-//                                     <a href="" class="search-link">
-//                                       <img  class="custom-card-image" src="./католог/Водолазка(normal)/1.jpg" alt="">
-//                                       <h5 class="card-title search-card-title">Водолазка(normal)</h5>
-//                                       <p class="card-text custom-card-text search-card-price">13,50$</p>
-
-//                                     </a>
-//                                   </li>
